f251b386d577cd50f851f7894a6670f379edbe03
Dominik Urban <109726930+DominikUrban231@users.noreply.github.com>
2024-02-06 00:47:52 +0100

    update
    styling + drop down menu + HTML code format

--- drop_down.js
+++ drop_down.js
@@ -1,13 +1,13 @@
-const dropDownMenus = document.querySelectorAll('.drop_down_menu');
+const dropDownMenus = document.querySelectorAll('#drop_down_menu');
 
 dropDownMenus.forEach(function(menu) {
     menu.addEventListener('mouseover', function() {
-        const submenu = menu.nextElementSibling;
+        const submenu = menu.children[1];
         submenu.classList.remove('hidden');
     });
 
     menu.addEventListener('mouseout', function() {
-        const submenu = menu.nextElementSibling;
+        const submenu = menu.children[1];
         submenu.classList.add('hidden');
     });
 });
